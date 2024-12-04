74f2b819fa1c6e8f7f0db5547228ba37fb873992
Sitch196 <sichinava1996@gmail.com>
2024-12-04 16:33:52 +0400

    read all films endpoint created

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const getFilmsData = () => {
+  const data = fs.readFileSync("top250.json", "utf8");
+  return JSON.parse(data);
+};
+
+app.get("/api/films/readall", (req, res) => {
+  try {
+    const films = getFilmsData();
+    const sortedFilms = films.sort((a, b) => a.position - b.position);
+    res.json(sortedFilms);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to read films data" });
+  }
 });
 
 app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+  console.log("Server is running on http://localhost:3000");
 });
